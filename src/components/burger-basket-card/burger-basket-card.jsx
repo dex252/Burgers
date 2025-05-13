d2a2f76c25502a755cdf52fb1f37f09697719c9a
Slava <dex252rus@gmail.com>
2025-05-13 20:30:33 +1000

    Правки в конструкторе бургеров (фиксация единственной булки снизу и сверху)

--- src/components/burger-basket-card/burger-basket-card.jsx
+++ src/components/burger-basket-card/burger-basket-card.jsx
@@ -5,14 +5,23 @@ import {
 	DragIcon,
 	CurrencyIcon,
 	LockIcon,
+	DeleteIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
+import PropTypes from 'prop-types';
 
-export const BurgerBasketCard = ({ ingredient }) => {
+export const BurgerBasketCard = ({ ingredient, postfix = undefined }) => {
 	console.log(ingredient);
+
+	/**
+	 * Тип ингредиента, не показываем drag-n-drop и блокируем возможность удалить ингредиент
+	 */
+	const isBunType = ingredient.type === 'bun';
 
 	return (
 		<section className={`${styles.card} mt-4 mb-4`}>
-			<DragIcon type='primary' className={styles.move_button}></DragIcon>
+			<DragIcon
+				type='primary'
+				className={`${styles.move_button} ${isBunType && styles.hidden}`}></DragIcon>
 			<div className={`${styles.card_content} ml-6`}>
 				<img
 					className='ml-4'
@@ -20,14 +29,19 @@ export const BurgerBasketCard = ({ ingredient }) => {
 					alt={ingredient.name}></img>
 				<p className={`text text_type_main-small ${styles.text}`}>
 					{ingredient.name}
+					{postfix}
 				</p>
 				<div className={`${styles.price} ml-5`}>
 					<p className='text text_type_digits-default'>{ingredient.price}</p>
 					<CurrencyIcon type='primary'></CurrencyIcon>
 				</div>
 				<div className={`${styles.ingredient_action} ml-5`}>
-					{/* От чего зависит изображение иконки? Могут ли несколько одинаковых продуктов находиться в корзине? */}
-					<LockIcon type='primary'></LockIcon>
+					{/* От чего зависит изображение иконки кроме, как булки? Могут ли несколько одинаковых продуктов находиться в корзине? */}
+					{isBunType ? (
+						<LockIcon type='secondary'></LockIcon>
+					) : (
+						<DeleteIcon type='primary'></DeleteIcon>
+					)}
 				</div>
 			</div>
 		</section>
@@ -36,4 +50,5 @@ export const BurgerBasketCard = ({ ingredient }) => {
 
 BurgerBasketCard.propTypes = {
 	ingredient: ingredientPropType.isRequired,
+	postfix: PropTypes.string,
 };
--- src/components/burger-contructor/burger-constructor.jsx
+++ src/components/burger-contructor/burger-constructor.jsx
@@ -7,23 +7,31 @@ import { OrderInfo } from '../order-info/order-info';
 
 export const BurgerConstructor = ({ ingredients }) => {
 	console.log(ingredients);
-	const totalPrice = ingredients.reduce((sum, ingredient) => {
-		return sum + ingredient.price * ingredient.__v;
-	}, 0);
+	const bun = ingredients.find((e) => e.type === 'bun');
+	const otherIngredients = ingredients.filter((e) => e.type !== 'bun');
+	const totalPrice =
+		otherIngredients.reduce((sum, ingredient) => {
+			return sum + ingredient.price;
+		}, 0) +
+		bun.price * 2;
 
 	return (
 		<section className={`${styles.burger_constructor} ml-4`}>
 			<div className={`${styles.basket_content} mb-10`}>
-				{ingredients.map(
+				<BurgerBasketCard key={bun._id} ingredient={bun} postfix='(верх)' />
+				{otherIngredients.map(
 					(ingredient) => (
 						<BurgerBasketCard key={ingredient._id} ingredient={ingredient} />
 					)
-					//TODO: Ограничить в зависимости от числа компонентов? А где это число хранится? В __v или требуется создать свою структуру для
-					//продуктов в корзине? А как их туда добавить? В фигме нет образца для переключателей, сво придумать?
+					//TODO: Ограничить в зависимости от числа добавленных ингредиентов?
+					// А где это число хранится? В __v или требуется создать свою структуру для
+					// продуктов в корзине? А как их туда добавить? В фигме нет образца для переключателей, свой придумать?
+					// или на текущем этапе это не требуется?
 					// ingredient.__v > 0 && (
 					// 	<BurgerBasketCard key={ingredient._id} ingredient={ingredient} />
 					// )
 				)}
+				<BurgerBasketCard key={bun._id} ingredient={bun} postfix='(низ)' />
 			</div>
 			<OrderInfo price={totalPrice}></OrderInfo>
 		</section>
