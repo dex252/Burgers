5e4be472d9ccfb10a902c05bd738288c84a07ac2
Slava <dex252rus@gmail.com>
2025-05-18 13:00:06 +1000

    Исправлен скроллинг в конструкторе. Изменил вывод карточек ингредиентов на библиотечный

--- src/components/burger-basket-card/burger-basket-card.jsx
+++ src/components/burger-basket-card/burger-basket-card.jsx
@@ -1,54 +1,29 @@
 import React from 'react';
 import styles from './burger-basket-card.module.css';
 import { ingredientPropType } from '@utils/prop-types.js';
-import {
-	DragIcon,
-	CurrencyIcon,
-	LockIcon,
-	DeleteIcon,
-} from '@ya.praktikum/react-developer-burger-ui-components';
-import PropTypes from 'prop-types';
+import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import * as PropTypes from 'prop-types';
 
-export const BurgerBasketCard = ({ ingredient, postfix = undefined }) => {
-	console.log(ingredient);
-
-	/**
-	 * Тип ингредиента, не показываем drag-n-drop и блокируем возможность удалить ингредиент
-	 */
-	const isBunType = ingredient.type === 'bun';
+export const BurgerBasketCard = ({ ingredient, type }) => {
+	const text =
+		ingredient.name + type === 'top'
+			? ' (верх)'
+			: type === 'bottom' && ' (низ)';
 
 	return (
 		<section className={`${styles.card} mt-4 mb-4`}>
-			<DragIcon
-				type='primary'
-				className={`${styles.move_button} ${isBunType && styles.hidden}`}></DragIcon>
-			<div className={`${styles.card_content} ml-6`}>
-				<img
-					className='ml-4'
-					src={ingredient.image_mobile}
-					alt={ingredient.name}></img>
-				<p className={`text text_type_main-small ${styles.text}`}>
-					{ingredient.name}
-					{postfix}
-				</p>
-				<div className={`${styles.price} ml-5`}>
-					<p className='text text_type_digits-default'>{ingredient.price}</p>
-					<CurrencyIcon type='primary'></CurrencyIcon>
-				</div>
-				<div className={`${styles.ingredient_action} ml-5`}>
-					{/* От чего зависит изображение иконки кроме, как булки? Могут ли несколько одинаковых продуктов находиться в корзине? */}
-					{isBunType ? (
-						<LockIcon type='secondary'></LockIcon>
-					) : (
-						<DeleteIcon type='primary'></DeleteIcon>
-					)}
-				</div>
-			</div>
+			<ConstructorElement
+				type={type}
+				isLocked={type !== undefined && true}
+				text={text}
+				price={ingredient.price}
+				alt={text}
+				thumbnail={ingredient.image_mobile}></ConstructorElement>
 		</section>
 	);
 };
 
 BurgerBasketCard.propTypes = {
 	ingredient: ingredientPropType.isRequired,
-	postfix: PropTypes.string,
+	type: PropTypes.oneOf(['top', 'bottom']),
 };
--- src/components/burger-contructor/burger-constructor.jsx
+++ src/components/burger-contructor/burger-constructor.jsx
@@ -18,21 +18,15 @@ export const BurgerConstructor = ({ ingredients }) => {
 	return (
 		<section className={`${styles.burger_constructor} ml-4`}>
 			<div className={`${styles.basket_content} mb-10`}>
-				<BurgerBasketCard key={bun._id} ingredient={bun} postfix='(верх)' />
-				{otherIngredients.map(
-					(ingredient) => (
+				<BurgerBasketCard ingredient={bun} type='top' />
+				<div className={`${styles.scroll_content}`}>
+					{otherIngredients.map((ingredient) => (
 						<BurgerBasketCard key={ingredient._id} ingredient={ingredient} />
-					)
-					//TODO: Ограничить в зависимости от числа добавленных ингредиентов?
-					// А где это число хранится? В __v или требуется создать свою структуру для
-					// продуктов в корзине? А как их туда добавить? В фигме нет образца для переключателей, свой придумать?
-					// или на текущем этапе это не требуется?
-					// ingredient.__v > 0 && (
-					// 	<BurgerBasketCard key={ingredient._id} ingredient={ingredient} />
-					// )
-				)}
-				<BurgerBasketCard key={bun._id} ingredient={bun} postfix='(низ)' />
+					))}
+				</div>
+				<BurgerBasketCard ingredient={bun} type='bottom' />
 			</div>
+
 			<OrderInfo price={totalPrice}></OrderInfo>
 		</section>
 	);
