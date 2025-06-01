de501e32ceb92dcbb5dd805b433744e89fe210ec
Slava <dex252rus@gmail.com>
2025-06-01 22:33:34 +1000

    Запрос api за номер заказа

--- src/services/api/yandex_api.jsx
+++ src/services/api/yandex_api.jsx
@@ -2,18 +2,31 @@ import axios from 'axios';
 
 const YANDEX_API = 'https://norma.nomoreparties.space';
 export const GET_INGREDIENTS = '/api/ingredients';
+export const GET_ORDER = '/api/orders';
 
 const api = axios.create({ baseURL: YANDEX_API });
 
-export const request = async (method) => {
+export const request = async (url, method = 'get', data = null) => {
 	try {
-		const response = await api.get(method);
+		const config = {
+			method: method.toLowerCase(),
+			url,
+		};
+
+		if (data) {
+			config.data = data;
+		}
+		const response = await api.request(config);
 		if (response.status === 200) {
 			return response.data;
 		}
 
 		throw new Error(`${response.status} ${response.statusText}`);
 	} catch (error) {
+		if (error.response && error.response.data && error.response.data.message) {
+			throw new Error(error.response.data.message);
+		}
+
 		throw new Error(error.message);
 	}
 };
