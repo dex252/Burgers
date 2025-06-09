5b7633c4470d2b58325ed50ef5bf9aa92546545f
Slava <dex252rus@gmail.com>
2025-06-09 20:40:24 +1000

    Подготовить концепцию запросов для выдачи токена

--- src/services/api/yandex_api.jsx
+++ src/services/api/yandex_api.jsx
@@ -7,30 +7,98 @@ export const _ERROR = '_ERROR';
 const YANDEX_API = 'https://norma.nomoreparties.space';
 export const GET_INGREDIENTS = '/api/ingredients';
 export const GET_ORDER = '/api/orders';
+export const LOGIN = '/api/orders';
+export const REGISTER = '/api/orders';
+export const LOGOUT = '/api/orders';
+export const GET_TOKEN = '/api/orders';
+export const GET_USER_DATA = '/api/auth/user';
 
 const api = axios.create({ baseURL: YANDEX_API });
 
-export const request = async (url, method = 'get', data = null) => {
+export const request = async (
+	url,
+	method = 'get',
+	data = null,
+	isToken = false
+) => {
+	const executeRequest = async () => {
+		try {
+			const config = {
+				method: method.toLowerCase(),
+				url,
+			};
+
+			if (data) {
+				config.data = data;
+			}
+
+			if (isToken) {
+				config.headers = {
+					Authorization:
+						'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MTYyNDI2MjJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_h5sfm_0',
+				};
+			}
+
+			const response = await api.request(config);
+			if (response.status === 200) {
+				return response.data;
+			}
+
+			if (response.status === 401 || response.status === 403) {
+				throw new Error(response.status);
+			}
+
+			throw new Error(`${response.status} ${response.statusText}`);
+		} catch (error) {
+			if (
+				error.response &&
+				(error.response.status == 401 || error.response.status == 403)
+			) {
+				throw new Error(error.response.status);
+			}
+
+			if (
+				error.response &&
+				error.response.data &&
+				error.response.data.message
+			) {
+				throw new Error(error.response.data.message);
+			}
+
+			throw new Error(error.message);
+		}
+	};
+
 	try {
-		const config = {
-			method: method.toLowerCase(),
-			url,
-		};
-
-		if (data) {
-			config.data = data;
-		}
-		const response = await api.request(config);
-		if (response.status === 200) {
-			return response.data;
-		}
-
-		throw new Error(`${response.status} ${response.statusText}`);
+		return await executeRequest();
 	} catch (error) {
-		if (error.response && error.response.data && error.response.data.message) {
-			throw new Error(error.response.data.message);
+		//Нестрогое неравенство
+		if (error.message != 401 && error.message != 403) {
+			throw new Error(error);
 		}
 
-		throw new Error(error.message);
+		await getAuth();
+		return await executeRequest();
 	}
+};
+
+const getAuth = async (
+	token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MTYyNDI2MjJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_h5sfm_0'
+) => {
+	await api
+		.request({
+			method: 'post',
+			url: GET_TOKEN,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		.then((e) => console.info(e))
+		.catch((e) => {
+			if (e.response && e.response.data && e.response.data.message) {
+				throw new Error(e.response.data.message);
+			}
+
+			throw new Error(e.message);
+		});
 };
