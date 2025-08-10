a184f60e32cbd6e87524eef577cddcc973524d72
Slava <dex252rus@gmail.com>
2025-08-10 18:00:22 +1000

    Страница об ошибке jsx=> tsx

--- src/pages/errors/404/index.tsx
+++ src/pages/errors/404/index.tsx
@@ -1,9 +1,11 @@
+import type { FC } from 'react';
+
 import styles from './index.module.css';
 
-export function ErrorPage404() {
+export const ErrorPage404: FC = () => {
 	return (
 		<section className={styles.content}>
 			<h1>Ошибка 404</h1>
 		</section>
 	);
-}
+};
