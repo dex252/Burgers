6dac2b5da7ce133384fe25625d4d7b4fc63c00f2
Slava <dex252rus@gmail.com>
2025-08-30 21:16:41 +1000

    Цветовая дифференциация заказов в ленте

--- src/components/feed/feed-list/feed-card/feed-card.tsx
+++ src/components/feed/feed-list/feed-card/feed-card.tsx
@@ -11,7 +11,16 @@ export const FeedCard = ({ order }: { order: HistoryOrder }): ReactElement => {
 	return (
 		<section className={styles.constructor_element}>
 			<div className={`${styles.card_header} ml-5 mt-6 mb-6`}>
-				<h1 className='text text_type_digits-default'>#{order.number}</h1>
+				<h1
+					className={`text text_type_digits-default ${
+						order.status == 'created'
+							? styles.card_order_created
+							: order.status == 'pending'
+								? styles.card_order_pending
+								: ''
+					}`}>
+					#{order.number}
+				</h1>
 				<FormattedDate
 					className='text text_type_main-default text_color_inactive'
 					date={new Date(order.createdAt)}
