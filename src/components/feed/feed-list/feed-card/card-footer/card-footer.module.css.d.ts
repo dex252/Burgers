02c29f75c71814e7aa73ff13b17899e694b31558
Slava <dex252rus@gmail.com>
2025-08-30 21:31:10 +1000

    Вычислить стоимость ингредиентов на карточке заказа

--- src/components/feed/feed-list/feed-card/card-footer/card-footer.module.css.d.ts
+++ src/components/feed/feed-list/feed-card/card-footer/card-footer.module.css.d.ts
@@ -1,4 +1,8 @@
 declare const classNames: {
-	readonly container: 'container';
+  readonly wrapper: "wrapper";
+  readonly container: "container";
+  readonly right_content: "right_content";
+  readonly currency_icon: "currency_icon";
+  readonly card_price: "card_price";
 };
 export default classNames;
--- src/components/feed/feed-list/feed-card/card-footer/card-footer.tsx
+++ src/components/feed/feed-list/feed-card/card-footer/card-footer.tsx
@@ -1,9 +1,10 @@
+import { CurrencyIcon } from '@krgaa/react-developer-burger-ui-components';
+import { useMemo, type ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 
 import { CardImage } from './card-image/card-image';
 
 import type { IngredientsReducerStates } from '@/utils/store-types';
-import type { ReactElement } from 'react';
 
 import styles from './card-footer.module.css';
 
@@ -35,23 +36,44 @@ export const CardFooter = ({
 			? ingredientDetails.length - MAX_VISIBLE_INGREDIENTS
 			: 0;
 
+	const price = useMemo(() => {
+		return ingredientDetails.reduce(
+			(sum, i) => sum + (Number(i?.price) || 0),
+			0
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ingredients]);
+
 	return (
-		<section className={`ml-6 mb-6 ${styles.container}`}>
-			{visibleIngredients.map((ingredient, index) => (
-				<div key={`${index}-${id}-${ingredient}`}>
+		<section className={`ml-6 mb-6 ${styles.wrapper}`}>
+			<div className={styles.container}>
+				{visibleIngredients.map((ingredient, index) => (
+					<div key={`${index}-${id}-${ingredient}`}>
+						<CardImage
+							src={ingredient?.image_mobile}
+							alt={ingredient?.name}></CardImage>
+					</div>
+				))}
+				{count > 0 && (
 					<CardImage
-						src={ingredient?.image_mobile}
-						alt={ingredient?.name}></CardImage>
+						src={ingredientDetails[MAX_VISIBLE_INGREDIENTS]?.image_mobile}
+						alt={ingredientDetails[MAX_VISIBLE_INGREDIENTS]?.name}
+						darkened={true}
+						text={`+${count}`}
+					/>
+				)}
+			</div>
+
+			{/* Иконка и текст справа */}
+			<div className={styles.right_content}>
+				<div className={`${styles.card_price} mb-1 mt-1`}>
+					<p className='text text_type_digits-default'>{price}</p>
+					<CurrencyIcon
+						type='primary'
+						className={`${styles.currency_icon} text text_type_digits-medium ml-6 mr-6`}
+					/>
 				</div>
-			))}
-			{count > 0 && (
-				<CardImage
-					src={ingredientDetails[MAX_VISIBLE_INGREDIENTS]?.image_mobile}
-					alt={ingredientDetails[MAX_VISIBLE_INGREDIENTS]?.name}
-					darkened={true}
-					text={`+${count}`}
-				/>
-			)}
+			</div>
 		</section>
 	);
 };
