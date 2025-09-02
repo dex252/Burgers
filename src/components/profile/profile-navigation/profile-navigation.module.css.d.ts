51474816a0fe487a8bc4833c724ec92faccc2e14
Slava <dex252rus@gmail.com>
2025-09-02 22:09:06 +1000

    Вынести панель навигация профиля в отдельный компонент

---
+++ src/components/profile/profile-navigation/profile-navigation.module.css.d.ts
@@ -0,0 +1,7 @@
+declare const classNames: {
+	readonly content_column: 'content_column';
+	readonly content_button: 'content_button';
+	readonly content_button_active: 'content_button_active';
+	readonly content_text: 'content_text';
+};
+export default classNames;
